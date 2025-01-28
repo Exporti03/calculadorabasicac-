bf3d491f70cad30db617cbdc47894b0ef5d91e2f
exporti03 <adzuluaga@outlook.com>
2025-01-27 23:02:36 -0500

    actualizacion1

---
+++ calculadora.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+
+int main() {
+
+    double numero1;
+    double numero2;
+    double resultado;
+    
+    std::cout << "#####CALCULADORA BASICA######\n";
+
+    std::cout << "Operacion de: (+)\n";
+    std::cout << "Operacion de: (-)\n";
+    std::cout << "Operacion de: (*)\n";
+    std::cout << "Operacion de: (/)\n";
+    
+    WHI
+
+    return 0;
+}
